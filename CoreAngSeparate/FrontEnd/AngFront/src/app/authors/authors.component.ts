bc000b7765f7be5d78ebb331993d0d662640593b
tarnaldo88 <tarnaldo88@gmail.com>
2020-12-11 11:35:05 -0800

    reverted back to front end working

---
+++ CoreAngSeparate/FrontEnd/AngFront/src/app/authors/authors.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Authors {
+  constructor(
+    public AuthorId: number,
+    public Name: string
+  ) {
+  }
+}
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+
+  authors: Authors[] = [];
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.getAuthors();
+  }
+
+  getAuthors(){
+    this.httpClient.get<any>('http://54.67.104.178/Server/api/Authors').subscribe(
+      response => {
+        console.log(response);
+        this.authors = response;
+      }
+    );
+  }
+
+}
